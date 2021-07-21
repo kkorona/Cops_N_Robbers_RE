611bcfd8b658756d649bec2f7cfd6bd07ee13271
kkorona <high1uck@pusan.ac.kr>
2021-07-21 18:34:37 +0900

    walla

--- engine_ntx.py
+++ engine_ntx.py
@@ -9,39 +9,52 @@ import gen_cylinder
 import rob_engine
 import cop_engine
 
-def viz_cylinder(G):
-    pos = dict( (n, n) for n in G.nodes() ) # pos=(위치, node 이름)
-    # vlabels = dict( ((i, j), i * 10 + j) for i, j in G.nodes() ) # 사용안함.
-    vlabels = dict( ((i, j), str(i)+","+str(j) ) for i, j in G.nodes() )
+N = 20
+M = 10
 
-    nx.draw_networkx(G, pos=pos, node_size=900,  labels=vlabels,node_color='tan', edge_color='r', font_size = 10)
+ncolor="gainsboro"
+ccolor="limegreen"
+rcolor="tomato"
+ecolor='gray'
+
+G = gen_cylinder.gen_cylinder(N,M)
+pos = dict( (n, n) for n in G.nodes() ) # pos=(위치, node 이름)
+vlabels = dict( ((i, j), str(i)+","+str(j) ) for i, j in G.nodes() )
+
+def viz_cylinder(G):
+    nx.draw_networkx(G, pos=pos, node_size=800,  labels=vlabels,node_color='tan', edge_color='r', font_size = 8)
     nx.draw_networkx_edges (G, pos,  width=2, edge_color='tan' )
 
 robber_state = []
 cops_state = []
 
-def draw_cops(G):
-    nx.draw(G, pos=pos, nodelist=cops_state, \
-                      node_size=900, node_color='yellow'  )
+def draw_agent(P, color):
+    nx.draw(G, pos=pos, nodelist=P, \
+                      node_size=800, node_color=color  )
+    
 
-def draw_robber(G)
+def draw_robber():
+    draw_agent(robber_state, rcolor)
+
+def draw_cops():
+    draw_agent(cops_state, ccolor)
                       
-def init_robber_state(G):
-    pass
+def init_robber_state():
+    robber_state.append((1,5))
 
-def init_cop_state(G):
-    pass
+def init_cop_state():
+    cops_state.append((7,4))
+    cops_state.append((11,9))
 
 def init():
-    robber_state.append((1,5))
-    cops_state.append((7,4))
-    cops_state.append((9,11))
+    init_robber_state()
+    init_cop_state()
 
-G = gen_cylinder.gen_cylinder(10,20)
-
-
-
-
+init()
 viz_cylinder(G)
+
+
+draw_cops()
+draw_robber()
 
 plt.show()
